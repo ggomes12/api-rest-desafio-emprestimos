753466cc67eea9d2725c0d3ab4bdfb90b1dd6e76
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-31 12:21:35 -0300

    :tada: init: first commit

---
+++ api-rest-desafio-emprestimo/src/main/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/ApiRestDesafioEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_desafio.api_rest_desafio_emprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDesafioEmprestimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDesafioEmprestimoApplication.class, args);
+	}
+
+}
---
+++ api-rest-desafio-emprestimo/src/test/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/ApiRestDesafioEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggomes.api_desafio.api_rest_desafio_emprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDesafioEmprestimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
