e5b48ec962f742cbde4b25795affa0caa548bb27
Guilherme Gomes <ggomes.jua.ti@gmail.com>
2025-01-31 14:50:28 -0300

    :scroll: log: Adding logs with Slf4j

--- api-rest-desafio-emprestimo/src/main/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/controller/LoanController.java
+++ api-rest-desafio-emprestimo/src/main/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/controller/LoanController.java
@@ -18,10 +18,12 @@ import com.ggomes.api_desafio.api_rest_desafio_emprestimo.services.LoanService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/customer-loans")
 @RequiredArgsConstructor
+@Slf4j
 public class LoanController {
 	
 	@Autowired
@@ -36,8 +38,10 @@ public class LoanController {
         
     public ResponseEntity<Map<String, Object>> getEligibleLoans(@RequestBody CustomerRequestDTO dto) {
     	
+    	log.info("Request received: {}", dto);
         List<LoanResponseDTO> loans = loanService.determineLoans(dto);
         
+        log.info("Loans determined: {}", loans);
         Map<String, Object> response = new HashMap<>();
         response.put("customer", dto.getName());
         response.put("loans", loans);
--- api-rest-desafio-emprestimo/src/main/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/services/LoanService.java
+++ api-rest-desafio-emprestimo/src/main/java/com/ggomes/api_desafio/api_rest_desafio_emprestimo/services/LoanService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import com.ggomes.api_desafio.api_rest_desafio_emprestimo.dtos.CustomerRequestDTO;
 import com.ggomes.api_desafio.api_rest_desafio_emprestimo.dtos.LoanResponseDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class LoanService{
 
 	public List<LoanResponseDTO> determineLoans(CustomerRequestDTO dto) {
@@ -16,22 +19,27 @@ public class LoanService{
 		int income = dto.getIncome();
 		String location = dto.getLocation();
 		
+		log.info("Determining loans for customer: {}", dto);
 		List<LoanResponseDTO> loans = new ArrayList<>();
 		
 		if (income <= 3000) {
 			loans.add(new LoanResponseDTO("PERSONAL", 4));
 			loans.add(new LoanResponseDTO("GUARANTEED", 3));
+			log.info("Added PERSONAL and GUARANTEED loans");
 		}
 		
 		if (income <= 5000) {
 			loans.add(new LoanResponseDTO("CONSIGNMENT", 2));
+			log.info("Added CONSIGNMENT loan");
 		}
 		
 		if (income > 3000 && income <= 5000 && age < 30 && "SP".equalsIgnoreCase(location)) {
 			loans.add(new LoanResponseDTO("PERSONAL", 4));
 			loans.add(new LoanResponseDTO("GUARANTEED", 3));
+			log.info("Added PERSONAL and GUARANTEED loans");
 		}
 		
+		log.info("Loans determined: {}", loans);
 		return loans;
 		
 	}
